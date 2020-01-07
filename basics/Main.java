c3236fd61a616da9a1e9e7ce4ba71f7df0807784
Jerome Joof <jeromejoof@yahoo.com>
2020-01-07 09:33:12 -0800

    pluralize function complete

---
+++ basics/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+  public static void main(String[] args) {
+
+    int dogCount = 1;
+    System.out.println("I own " + dogCount + " " + pluralize("dog", dogCount) + ".");
+    
+    int catCount = 2;
+    System.out.println("I own " + catCount + " " + pluralize("cat", catCount) + ".");
+    
+    int turtleCount = 0;
+    System.out.println("I own " + turtleCount + " " + pluralize("turtle", turtleCount) + ".");
+   
+  }
+
+  //Create a pluralize function
+  public static String pluralize(String word, int num){
+
+    if(num >= 2 || num == 0){
+      return word + "s";
+    } else {
+      return word;
+    }
+      
+  }
+
+
+
+  
+}
+
