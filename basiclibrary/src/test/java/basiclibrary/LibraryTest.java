3585e3a5544f04977d00ae8914e3e563cdbc2584
Jerome Joof <jeromejoof@yahoo.com>
2020-01-09 13:47:42 -0800

    update

--- basiclibrary/src/test/java/basiclibrary/LibraryTest.java
+++ basiclibrary/src/test/java/basiclibrary/LibraryTest.java
@@ -5,11 +5,18 @@ package basiclibrary;
 
 import org.junit.Test;
 
-import java.util.Arrays;
 
-import static org.junit.Assert.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static basiclibrary.Library.tally;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class LibraryTest {
+
+
     @Test public void rollTest(){
         int[] rollDice = new int[4];
         int diceLength = rollDice.length;
@@ -36,6 +43,34 @@ public class LibraryTest {
         String expecting = Arrays.toString(weeklyMonthTemperatures[2]);
         String result = Library.averageArrayValue(weeklyMonthTemperatures);
         assertEquals(result, expecting);
+    }
+    @Test public void testArrayMinMaxValue(){
+        int[][] weeklyMonthTemperatures = {
+                {66, 64, 58, 65, 71, 57, 60},
+                {57, 65, 65, 70, 72, 65, 51},
+                {55, 54, 60, 53, 59, 57, 61},
+                {65, 56, 55, 52, 55, 62, 57}
+        };
+        Library testArray = new Library();
+        String msgOutput = "High: 72\n Low: 51\n Never saw temperature: 63\n Never saw temperature: 67\n Never saw temperature: 68\n Never saw temperature: 69\n";
+        System.out.println(testArray.arrayMinMaxValue(weeklyMonthTemperatures));
+        System.out.println(testArray.arrayMinMaxValue(weeklyMonthTemperatures).equals(msgOutput));
+    }
+    @Test public void testTallyVotes(){
+        List<String> votes = new ArrayList<>();
+        votes.add("Bush");
+        votes.add("Bush");
+        votes.add("Bush");
+        votes.add("Shrub");
+        votes.add("Hedge");
+        votes.add("Shrub");
+        votes.add("Bush");
+        votes.add("Hedge");
+        votes.add("Bush");
+
+        String winner = tally(votes);
+        System.out.println(winner + " received the most votes!");
+        assertEquals("The Winner is: ", "Bush", tally(votes));
     }
 
 }
